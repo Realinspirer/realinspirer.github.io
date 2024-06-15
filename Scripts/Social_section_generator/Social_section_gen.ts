2148c71cf9b799cc1b7b41705c48cc85de2ab245
Realinspirer <realinspirer@outlook.com>
2024-06-16 02:57:10 +0530

    Updated sited

---
+++ Scripts/Social_section_generator/Social_section_gen.ts
@@ -0,0 +1,43 @@
+class normal_section_class_no_desc{
+    title: string;
+    subtitle: string;
+    image_url: string;
+    button_text: string;
+    click_url: string;
+    constructor(title:string, sub_title:string, des:string,
+         img:string, btn_txt:string, cl_url:string){
+        this.title = title;
+        this.subtitle = sub_title;
+        this.image_url = img;
+        this.button_text = btn_txt;
+        this.click_url = cl_url;
+    }
+}
+
+
+get_json_social("\\Scripts\\Social_section_generator\\Social_section_gen.json", "socials_grid");
+
+async function get_json_social(json_loc:string, id_grid:string, window_argument:string="") {
+
+    var response = await fetch(json_loc);
+    var res_ar:Array<normal_section_class_no_desc> = await response.json();
+
+    res_ar.reverse().forEach(res => {
+        var items = 
+    `<div class="social_section">` +
+        `<img class="social_img" src="${res.image_url}" alt="social_img">` +
+        `<div class="back_grad">` +
+            `<div class="social_texts">` +
+                `<p class="title">${res.title}</p>`+
+                `<p class="subtitle">${res.subtitle}</p>`+
+                `<a class="normal_button" onclick="window.open('${res.click_url}','${window_argument}')">${res.button_text}</a>`+
+            `</div>` +
+        `</div>` +
+
+    `</div>`
+
+    $(`#${id_grid}`).prepend(items);
+
+    });
+    
+}
